d145cdd5a20725874ca418787618ebbf2bbfe114
Pablo <pablo.dominguez@iescamas.es>
2025-01-17 10:39:23 +0100

    semilla

---
+++ termometro_proyecto/src/test/java/com/endes/instrumento/TermometroTest.java
@@ -0,0 +1,14 @@
+package com.endes.instrumento;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TermometroTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
